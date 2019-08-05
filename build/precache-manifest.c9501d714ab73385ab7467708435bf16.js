a71035fd5c0445285dc1582cfc2837851726a3e5
Bach Ngoc Tinh <winanh2151993@gmail.com>
2019-08-05 17:02:40 +0700

    build

---
+++ build/precache-manifest.c9501d714ab73385ab7467708435bf16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5db3c19ec43bd728ec72f2a264705da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "678b9cc45300d5476cc0",
+    "url": "/static/css/main.dfc1b3c2.chunk.css"
+  },
+  {
+    "revision": "73c8e9da521413dfd5c3",
+    "url": "/static/js/2.a9a63109.chunk.js"
+  },
+  {
+    "revision": "678b9cc45300d5476cc0",
+    "url": "/static/js/main.f14fc214.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
